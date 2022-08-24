baf8eb2d4c97cfc9470f64c40208ef7f6cb9f3bc
Prabir Singh <codeprabir@gmail.com>
2022-08-25 03:09:14 +0530

    Create Login API

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,14 @@
+import { useSession } from 'next-auth/react'
 import Head from 'next/head'
 import Link from 'next/link'
 import React, { useContext, useEffect, useState } from 'react'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { Store } from '../utils/Store'
 
 export default function Layout({ title, children }) {
+  const { status, data: session } = useSession()
+
   const { state } = useContext(Store)
   const { cart } = state
   const [cartItemsCount, setCartItemsCount] = useState(0)
@@ -18,6 +23,7 @@ export default function Layout({ title, children }) {
         <meta name='description' content='Bangladeshi Shope in Prague' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <ToastContainer position='bottom-center' limit={1} />
       <div className='flex min-h-screen flex-col justify-between'>
         <header>
           <nav className='flex h-12 items-center px-4 justify-between shadow-md'>
@@ -28,9 +34,16 @@ export default function Layout({ title, children }) {
               <Link href='/register'>
                 <a className='p-2'>Register</a>
               </Link>
-              <Link href='/login'>
-                <a className='p-2'> Login</a>
-              </Link>
+
+              {status === 'loading' ? (
+                'Loading'
+              ) : session?.user ? (
+                session.user.name
+              ) : (
+                <Link href='/login'>
+                  <a className='p-2'>Login</a>
+                </Link>
+              )}
               <Link href='/cart'>
                 <a className='p-2'>
                   {/* <ShoppingCartIcon className="h-4 w-4 "/> */}
