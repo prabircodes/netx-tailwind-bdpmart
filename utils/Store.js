fe64b33c8adc46a73cb30c8456e8a997640ee6c3
Prabir Singh <codeprabir@gmail.com>
2022-08-25 14:59:53 +0530

    Payment Method Done

--- utils/Store.js
+++ utils/Store.js
@@ -39,17 +39,25 @@ function reducer(state, action) {
           paymentMethod: '',
         },
       }
-      case 'SAVE_SHIPPING_ADDRESS':
-        return {
-          ...state,
-          cart: {
-            ...state.cart,
-            shippingAddress: {
-              ...state.cart.shippingAddress,
-              ...action.payload,
-            },
+    case 'SAVE_SHIPPING_ADDRESS':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: {
+            ...state.cart.shippingAddress,
+            ...action.payload,
           },
-        };
+        },
+      }
+    case 'SAVE_PAYMENT_METHOD':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          paymentMethod: action.payload,
+        },
+      }
     default:
       return state
   }
